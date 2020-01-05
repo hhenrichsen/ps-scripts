1f07bbcd03879bd88f2449d6b165004efffb3d89
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2020-01-04 17:34:09 -0700

    Handle changes so we don't repeat things.

--- resize.ps1
+++ resize.ps1
@@ -1,8 +1,12 @@
 $maxSize = 1000kb
+$changedFiles = @()
 # Move anything that we're done with into the old/ folder.
 Get-ChildItem '.\resized\*.jpg' -File | ForEach-Object {Move-Item -Path $_.FullName -Destination ('old\' + $_.Name)}
 # Convert pdfs to jpgs.
-Get-ChildItem *.pdf -File | ForEach-Object {magick $_.FullName ($_.BaseName + '.jpg')}
+Get-ChildItem *.pdf -File | ForEach-Object {
+    $changedFiles += $_
+    magick $_.FullName ($_.BaseName + '.jpg')
+}
 # Loop through the jpgs.
 Get-ChildItem *.jpg -File | ForEach-Object {
     # Create a name for the new file in the resized/ folder.
@@ -13,6 +17,11 @@ Get-ChildItem *.jpg -File | ForEach-Object {
     $newFile = Get-Item $newFilePath
     # Check if the new file is bigger. If it is, use the old one.
     if ( $newFile.Length -gt $_.Length) {
-        Move-Item -Force -Path $_.FullName -Destination $newfilePath
+        Copy-Item -Force -Path $_.FullName -Destination $newfilePath
     }
+    $changedFiles += $_
+}
+# Move things out of the way so we don't repeat ourselves.
+$changedFiles | ForEach-Object {
+    Move-Item -Path $_.FullName -Destination ('source\' + $_.Name)
 }
